790fce89a5f0e1539f51bb41e7d434441c012e29
Wesley H. de Brito <wesleyheringer24@gmail.com>
2025-08-25 21:55:44 -0300

    Implements data structure and board display

---
+++ Basic_game.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void initializeBoard(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ' ';
+            printf("|%c", board[i][j]);
+        }
+        printf("|\n");
+    }
+}
+
+
+int main() {
+    
+    char board[3][3];
+    int options;
+
+    printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+    printf("+-+-+-+-+| WELCOME TO TIC TAC TOE! |+-+-+-+-+\n");
+    printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+    printf("+-+-+-+-+-+-+-| 1 >>PLAY<< 1 |-+-+-+-+-+-+-+-\n");
+    printf("+-+-+-+-+-+-+-| 2 >>EXIT<< 2 |-+-+-+-+-+-+-+-\n");
+    printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+    printf("Enter how you wish to proceed: ");
+    scanf("%d", &options);
+
+    if (options == 1){
+        initializeBoard(board);
+    } else if (options == 2){
+        printf("Closing the menu...");
+    } else{
+        printf("Invalid response!");
+    }
+
+
+    return 0;
+    
+}
