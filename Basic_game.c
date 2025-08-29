ec3e04ef1f3b4e9e372eaa91973b6cbf473a9287
Wesley H. de Brito <wesleyheringer24@gmail.com>
2025-08-29 00:22:19 -0300

    Refactored the initializeBoard() function and created a function to print the board. Created the choice menu loop.

--- Basic_game.c
+++ Basic_game.c
@@ -5,32 +5,45 @@ void initializeBoard(char board[3][3]) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             board[i][j] = ' ';
+        }
+    }
+}
+// Function to print the Tic-Tac-Toe board to the screen, still empty
+void printBoard(char board[3][3]) {
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
             printf("|%c", board[i][j]);
         }
-        printf("|\n");
+        printf("\n");
     }
 }
 
 int main() {
     
     char board[3][3]; // Matrix that representing the board
-    int options; //
+    int options; 
 
     printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
     printf("+-+-+-+-+| WELCOME TO TIC TAC TOE! |+-+-+-+-+\n");
     printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
-    printf("+-+-+-+-+-+-+-| 1 >>PLAY<< 1 |-+-+-+-+-+-+-+-\n");
-    printf("+-+-+-+-+-+-+-| 2 >>EXIT<< 2 |-+-+-+-+-+-+-+-\n");
-    printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
-    printf("Enter how you wish to proceed: ");
-    scanf("%d", &options);
+    // Loop for the options menu, repeating until the start or exit option is entered
+    while (1) {
+        printf("Enter how you wish to proceed:\n");
+        printf("+-+-+-+-+-+-+-| 1 >>PLAY<< 1 |-+-+-+-+-+-+-+-\n");
+        printf("+-+-+-+-+-+-+-| 2 >>EXIT<< 2 |-+-+-+-+-+-+-+-\n");
+        printf(">>> ");
+        scanf("%d", &options);
 
-    if (options == 1){
-        initializeBoard(board);
-    } else if (options == 2){
-        printf("Closing the menu...");
-    } else{
-        printf("Invalid response!");
+        if (options == 1){
+            initializeBoard(board);
+            printBoard(board);
+            return 1;
+        } else if (options == 2) {
+            printf("Closing the game...\n");
+            break;
+        } else {
+            printf("Invalid selection! Please enter the option again.\n");
+        }
     }
 
     return 0;
